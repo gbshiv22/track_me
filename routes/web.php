fd7906f35d39fe2ee8c54316635631af92cf9baf
gbshiv22 <112764036+gbshiv22@users.noreply.github.com>
2025-10-20 00:13:42 +0530

    Redirect homepage to login page as workaround for Render 500 error

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Web\AnalyticsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 // Health check route for Render
