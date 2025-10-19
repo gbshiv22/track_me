34c6721d694f263488f202f5e5f6e2b137172c27
gbshiv22 <112764036+gbshiv22@users.noreply.github.com>
2025-10-19 23:56:15 +0530

    Add test route to debug homepage issue

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Web\AnalyticsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+    return 'Test route is working!';
+});
+
 Route::get('/', function () {
     return 'Hello World! Track Me is working!';
 });
